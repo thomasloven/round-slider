3f216ccb101faef50d6655d676f7c5020e0aeec2
Paulus Schoutsen <balloob@gmail.com>
2019-09-04 13:28:34 -0700

    Add ES export

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,38 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import minify from 'rollup-plugin-babel-minify';
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import minify from "rollup-plugin-babel-minify";
 
-export default {
-  input: './src/main.js',
-  output: {
-    file: './round-slider.js',
-    format: 'iife',
-    compact: true,
+export default [
+  {
+    input: "./src/main.js",
+    output: {
+      file: "./round-slider.js",
+      format: "es",
+      compact: true
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      resolve(),
+      minify({comments: false}),
+    ]
   },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    resolve(),
-    minify({comments: false}),
-  ],
-};
+  {
+    input: "./src/main.js",
+    output: {
+      file: "./round-slider.es.js",
+      format: "es",
+      compact: true
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**",
+        externalHelpers: true,
+      })
+    ],
+    "external": [
+      "lit-element"
+    ]
+  }
+];
